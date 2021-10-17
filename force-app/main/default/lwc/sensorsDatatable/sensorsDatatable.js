e6b8d17902757b8dc2ac759f3e73d36ee5cd2dfd
y0ungleb <gleb.goose@gmail.com>
2021-10-18 02:35:01 +0300

    Ready to real development

--- force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
+++ force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
@@ -1,9 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-const columns = [
-	{ label: 'Sesnor Model', fieldName: 'Sensor_Model__c' },
-	{ label: 'Status', fieldName: 'Status__c', type: 'url' },
-	{ label: 'Assigned Base Station', fieldName: 'Base_Station__c'},
-];
+export default class SensorsDatatable extends LightningElement {
+	columns = [
+		{ label: 'Sesnor Model', fieldName: 'Sensor_Model__c', type: 'text' },
+		{ label: 'Status', fieldName: 'Status__c', type: 'url', type: 'text' },
+		{ label: 'Assigned Base Station', fieldName: 'Base_Station__c', type: 'text'}
+	];
 
-export default class SensorsDatatable extends LightningElement {}
+	testData = [
+		{
+			Sensor_Model__c: "XC5",
+			Status__c : "Active",
+			Base_Station__c : "Masherova 19 Minsk"
+		},
+		{
+			Sensor_Model__c: "XC6",
+			Status__c : "Inactive",
+			Base_Station__c : "Masherova 35 Minsk"
+		},
+		{
+			Sensor_Model__c: "XC7",
+			Status__c : "Inactive",
+			Base_Station__c : "Masherova 100 Minsk"
+		}
+	];
+}

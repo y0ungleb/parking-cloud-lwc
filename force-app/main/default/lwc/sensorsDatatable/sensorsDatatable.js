29d74362db1fb84741aa8f964e35bfa6250663f1
y0ungleb <gleb.goose@gmail.com>
2021-10-18 01:26:49 +0300

    First template code

--- force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
+++ force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
+const columns = [
+	{ label: 'Sesnor Model', fieldName: 'Sensor_Model__c' },
+	{ label: 'Status', fieldName: 'Status__c', type: 'url' },
+	{ label: 'Assigned Base Station', fieldName: 'Base_Station__c'},
+];
+
 export default class SensorsDatatable extends LightningElement {}

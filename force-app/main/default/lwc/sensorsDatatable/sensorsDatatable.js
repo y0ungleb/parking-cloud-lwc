106903fad9e2ddaee61984e8c3e12ed820118902
y0ungleb <gleb.goose@gmail.com>
2021-10-18 22:03:07 +0300

    Test code ready

--- force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
+++ force-app/main/default/lwc/sensorsDatatable/sensorsDatatable.js
@@ -3,10 +3,14 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import importRecordsFromFile from '@salesforce/apex/UploadController.importRecordsFromFile';
 
 export default class SensorsDatatable extends LightningElement {
+	@api recordid;
+	@track data;
 	@track showLoadingSpinner = false;
 	MAX_CSV_SIZE = 1500000;
 	fileUploaded;
 	fileUploadedName;
+	fileReader;
+	fileContents;
 	file;
 
 
@@ -16,23 +20,23 @@ export default class SensorsDatatable extends LightningElement {
 		{ label: 'Assigned Base Station', fieldName: 'Base_Station__c', type: 'text'}
 	];
 
-	testData = [
-		{
-			Sensor_Model__c: "XC5",
-			Status__c : "Active",
-			Base_Station__c : "Masherova 19 Minsk"
-		},
-		{
-			Sensor_Model__c: "XC6",
-			Status__c : "Inactive",
-			Base_Station__c : "Masherova 35 Minsk"
-		},
-		{
-			Sensor_Model__c: "XC7",
-			Status__c : "Inactive",
-			Base_Station__c : "Masherova 100 Minsk"
-		}
-	];
+	// testData = [
+	// 	{
+	// 		Sensor_Model__c: "XC5",
+	// 		Status__c : "Active",
+	// 		Base_Station__c : "Masherova 19 Minsk"
+	// 	},
+	// 	{
+	// 		Sensor_Model__c: "XC6",
+	// 		Status__c : "Inactive",
+	// 		Base_Station__c : "Masherova 35 Minsk"
+	// 	},
+	// 	{
+	// 		Sensor_Model__c: "XC7",
+	// 		Status__c : "Inactive",
+	// 		Base_Station__c : "Masherova 100 Minsk"
+	// 	}
+	// ];
 
 	handleUpload(event) {
 		this.fileUploaded = event.target.files;
@@ -40,6 +44,46 @@ export default class SensorsDatatable extends LightningElement {
 	}
 
 	handleInsert() {
+		if(this.fileUploaded.length() > 0) {
+			this.uploadHelper();
+		}
+		else {
+			//Toast message
+		}
+	}
 
+	uploadHelper() {
+		this.file = this.fileUploaded;
+		if(this.file.length() > this.MAX_CSV_SIZE) {
+			//Toast message
+			return;
+		}
+		else {
+			this.showLoadingSpinner = true;
+			this.fileReader = new FileReader();
+			this.fileReader.onloadend = (() => {
+				this.fileContents = this.fileReader.result;
+				this.saveToFile();
+			});
+
+			this.fileReader.readAsText(this.file);
+		}
+	}
+
+	saveToFile() {
+		importRecordsFromFile({rawData: JSON.stringify(this.fileContents), cdbId: this.recordid})
+		.then(result => {
+			this.data = result;
+			this.showLoadingSpinner = false;
+			this.dispatchEvent(
+				//Toast event
+			)
+		})
+
+		.catch(error => {
+			this.dispatchEvent(
+				//Toast event
+			)
+		})
 	}
 }
